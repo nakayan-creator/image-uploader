654e356d5ba554695e097d1ad50d06ba1bea1201
nakayan-creator <63556889+nakayan-creator@users.noreply.github.com>
2020-09-25 15:08:20 +0900

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {MuiThemeProvider} from '@material-ui/core'
+import {theme} from './assets/theme'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MuiThemeProvider theme={theme} >
+      <App style={{backgroundColor: '#f5f5f5'}} />
+    </MuiThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
