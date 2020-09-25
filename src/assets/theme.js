8d77e45f38d348ae8839b036c6d697438db62aea
nakayan-creator <63556889+nakayan-creator@users.noreply.github.com>
2020-09-25 15:09:02 +0900

    Add files via upload

---
+++ src/assets/theme.js
@@ -0,0 +1,17 @@
+import {createMuiTheme} from '@material-ui/core/styles'
+
+export const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#88ffff',
+            main: '#4dd0e1'
+        },
+        secondary: {
+            light: '#ffff81',
+            main: '#ffd54f'
+        }
+        // grey: {
+
+        // }
+    }
+})
